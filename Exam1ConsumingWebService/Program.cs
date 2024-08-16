2793ac4f24159abfc31f0ebdaeca7c036501ce31
Nelson Isidro Jr. <isidronelson454@gmail.com>
2024-08-16 16:35:47 +0800

    Added Deserialize Json and WeatherDataJson data

--- Exam1ConsumingWebService/Program.cs
+++ Exam1ConsumingWebService/Program.cs
@@ -1,13 +1,21 @@
 ﻿using Newtonsoft.Json;
 
-
 namespace Exam1ConsumingWebService
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
             WeatherApiHelper weatherApiHelper = new WeatherApiHelper();
+
+            string json = weatherApiHelper.GetWeatherData();
+
+            WeatherDataJson weatherDataJson = JsonConvert.DeserializeObject<WeatherDataJson>(json);
+
+            Console.WriteLine($"Temperature: {weatherDataJson.main.temp}");
+            Console.WriteLine($"Humidity: {weatherDataJson.main.humidity}");
+            Console.WriteLine($"Wind Speed: {weatherDataJson.wind.speed}");
 
         }
     }
